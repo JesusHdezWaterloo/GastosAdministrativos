921ad9ac43ef13f7cba21b110b7495aa4f12c64d
Unknown <unknown@example.com>
2020-08-28 23:59:16 -0500

    Movimiento reporte gasto

--- src/main/java/com/jhw/gestion/modules/gasto/core/usecase_impl/GastoUseCaseImpl.java
+++ src/main/java/com/jhw/gestion/modules/gasto/core/usecase_impl/GastoUseCaseImpl.java
@@ -1,10 +1,14 @@
 package com.jhw.gestion.modules.gasto.core.usecase_impl;
 
+import com.jhw.gestion.modules.contabilidad.core.domain.MonedaDomain;
 import com.jhw.gestion.modules.contabilidad.core.usecase_impl.DefaultPagableUseCaseImpl;
+import com.jhw.gestion.modules.contabilidad.utils.MonedaHandler;
 import com.jhw.gestion.modules.gasto.core.domain.*;
 import com.jhw.gestion.modules.gasto.core.module.GastoCoreModule;
 import com.jhw.gestion.modules.gasto.core.repo_def.*;
 import com.jhw.gestion.modules.gasto.core.usecase_def.*;
+import java.math.BigDecimal;
+import java.util.HashMap;
 
 public class GastoUseCaseImpl extends DefaultPagableUseCaseImpl<GastoDomain> implements GastoUseCase {
 
@@ -12,5 +16,23 @@ public class GastoUseCaseImpl extends DefaultPagableUseCaseImpl<GastoDomain> imp
 
     public GastoUseCaseImpl() {
         super.setRepo(repo);
+    }
+
+    @Override
+    public HashMap<TipoGastoDomain, BigDecimal> reporteGastadoPorGasto() throws Exception {
+        HashMap<TipoGastoDomain, BigDecimal> h = new HashMap<>();
+        MonedaDomain m = MonedaHandler.getMonedaBase();
+        if (m == null) {
+            return h;
+        }
+        for (TipoGastoDomain tipoGastoDomain : GastoCoreModule.getInstance().getImplementation(TipoGastoUseCase.class).findAll()) {
+            h.put(tipoGastoDomain, BigDecimal.ZERO);
+        }
+        for (GastoDomain g : GastoCoreModule.getInstance().getImplementation(GastoUseCase.class).findAll()) {
+            TipoGastoDomain tipo = g.getTipoGastoFk();
+            BigDecimal val = h.get(tipo).add(MonedaHandler.compra(g.getValor(), g.getMonedaFk(), m));
+            h.put(tipo, val);
+        }
+        return h;
     }
 }
