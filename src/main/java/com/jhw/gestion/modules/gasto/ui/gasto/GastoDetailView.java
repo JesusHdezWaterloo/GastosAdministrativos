11d9a75583e64a4aefd191c59be86f12edff9701
Unknown <unknown@example.com>
2020-09-02 23:19:22 -0500

    *All: fix SDF

--- src/main/java/com/jhw/gestion/modules/gasto/ui/gasto/GastoDetailView.java
+++ src/main/java/com/jhw/gestion/modules/gasto/ui/gasto/GastoDetailView.java
@@ -3,9 +3,6 @@ package com.jhw.gestion.modules.gasto.ui.gasto;
 import com.clean.core.app.services.ExceptionHandler;
 import com.jhw.gestion.modules.gasto.ui.module.GastoSwingModule;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import com.jhw.swing.material.standards.MaterialIcons;
-import com.jhw.swing.material.components.button._MaterialButtonIconTransparent;
 import com.jhw.swing.models.detail._MaterialPanelDetail;
 import com.jhw.swing.material.components.table.Column;
 import com.jhw.swing.models.input.dialogs.DialogModelInput;
@@ -15,7 +12,7 @@ import com.jhw.swing.material.components.table.editors_renders.money.MoneyTableC
 import com.jhw.gestion.modules.gasto.core.domain.GastoDomain;
 import com.jhw.gestion.modules.gasto.ui.chart.GastosChart;
 import com.jhw.gestion.modules.gasto.ui.module.GastoModuleNavigator;
-import static com.jhw.utils.others.SDF.SDF;
+import com.jhw.utils.others.SDF;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import javax.swing.AbstractAction;
@@ -70,7 +67,7 @@ public class GastoDetailView extends _MaterialPanelDetail<GastoDomain> implement
         return new Object[]{
             obj.getTipoGastoFk(),
             MoneyTableComponent.from(obj.getValor(), obj.getMonedaFk()),
-            SDF.format(obj.getCuadreFk().info().getFecha()),
+            SDF.SDF.format(obj.getCuadreFk().info().getFecha()),
             obj.getCuadreFk().getOperacionContableFk().getCuentaFk()};
     }
 
