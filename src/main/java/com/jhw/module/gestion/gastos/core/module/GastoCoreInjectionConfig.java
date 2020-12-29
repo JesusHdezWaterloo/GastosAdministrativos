ecf6c5b6ce66b2fd6c2f9d3361ffc059205be9a0
Unknown <unknown@example.com>
2020-12-28 23:37:56 -0500

    +Licence

--- src/main/java/com/jhw/module/gestion/gastos/core/module/GastoCoreInjectionConfig.java
+++ src/main/java/com/jhw/module/gestion/gastos/core/module/GastoCoreInjectionConfig.java
@@ -2,19 +2,21 @@ package com.jhw.module.gestion.gastos.core.module;
 
 import com.jhw.module.gestion.gastos.core.usecase_def.TipoGastoUseCase;
 import com.jhw.module.gestion.gastos.core.usecase_def.GastoUseCase;
-import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 import com.jhw.module.gestion.gastos.core.usecase_impl.*;
+import com.jhw.module.util.licence.core.injection.LicenceInjectionConfig;
 
 /**
  * Configuracion del injection del modulo de gasto-core.
  *
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  */
-public class GastoCoreInjectionConfig extends AbstractModule {
+public class GastoCoreInjectionConfig extends LicenceInjectionConfig {
 
     @Override
     protected void configure() {
+        super.configure();//configura la licencia
+        
         bind(GastoUseCase.class).to(GastoUseCaseImpl.class).in(Singleton.class);
         bind(TipoGastoUseCase.class).to(TipoGastoUseCaseImpl.class).in(Singleton.class);
     }
