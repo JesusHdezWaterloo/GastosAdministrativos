4b4e485b6a34a634cfb3a46904d3adc12b2e8af3
Unknown <unknown@example.com>
2021-01-04 14:43:11 -0500

    Refactor a licencia para moverla a control

--- src/main/java/com/root101/module/gestion/gastos/core/module/GastoCoreInjectionConfig.java
+++ src/main/java/com/root101/module/gestion/gastos/core/module/GastoCoreInjectionConfig.java
@@ -20,7 +20,7 @@ import com.root101.module.gestion.gastos.core.usecase_def.TipoGastoUseCase;
 import com.root101.module.gestion.gastos.core.usecase_def.GastoUseCase;
 import com.google.inject.Singleton;
 import com.root101.module.gestion.gastos.core.usecase_impl.*;
-import com.jhw.module.util.licence.core.injection.LicenceInjectionConfig;
+import com.root101.module.control.licence.core.injection.LicenceInjectionConfig;
 
 /**
  *
